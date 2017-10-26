850cc52e80d7f522ee2d268a7eeb22067da95ffa
Panut Prempreecha <slowair6@hotmail.com>
2017-10-26 18:39:40 +0700

    First commit

--- routes/index.js
+++ routes/index.js
@@ -2,8 +2,7 @@ var express = require('express');
 var router = express.Router();
 var cards = require('./schemaCards');
 
-/* GET home page. */
-router.get('/update', function (req, res, next) {
+router.get('/pay', function (req, res, next) {
     cards.findOne({card_number: req.query.card_number, card_type :req.query.card_type}, function (err, foundACC) {
         if (err) {
             
@@ -36,6 +35,44 @@ router.get('/update', function (req, res, next) {
                         } else {
                             console.log('Updated coin amount');
                             var data = {success: true, detail: "ชำระเงินแล้ว"};
+                            res.json(data);
+                        }
+                    });
+                }
+            }
+        }
+    });
+});
+
+router.get('/return', function (req, res, next) {
+    cards.findOne({card_number: req.query.card_number, card_type :req.query.card_type}, function (err, foundACC) {
+        if (err) {
+            
+        } else {
+            if (!foundACC) {
+                console.log("ไม่พบบัตรดังกล่าว");
+                var data = {success: false, detail: "ข้อมูลบัตรไม่ถูกต้อง"};
+                res.json(data);
+            } else {
+                console.log("Data : ");
+                console.log(foundACC);
+                console.log("Attemp : ");
+                console.log(req.query);
+                if (foundACC.csc !== req.query.csc || foundACC.card_expire !== req.query.card_expire) {
+                    console.log(foundACC.csc + " vs " + req.query.csc);
+                    console.log(foundACC.card_expire + " vs " + req.query.card_expire);
+                    console.log("ข้อมูลบัตรไม่ถูกต้อง");
+                    var data = {success: false, detail: "ข้อมูลบัตรไม่ถูกต้อง"};
+                    res.json(data);
+                } else {
+                    foundACC.balance = foundACC.balance + req.query.amount;
+                    foundACC.save(function (err, updateObject) {
+                        if (err) {
+                            console.log(err);
+                            res.status(500).send();
+                        } else {
+                            console.log('Updated coin amount');
+                            var data = {success: true, detail: "ทำการโอนเงินเข้าบัญชีคุณเรียบร้อยแล้ว"};
                             res.json(data);
                         }
                     });
